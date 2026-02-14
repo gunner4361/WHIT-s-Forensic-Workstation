cc539aba3bc650c952f0574e219a9a64c15549c4
gunner4361 <whittaker4361@gmail.com>
2026-02-14 12:12:01 -0500

    Add files via upload

---
+++ WHIT-update.ps1
@@ -0,0 +1,11 @@
+# Install the Windows Update module
+Install-Module -Name PSWindowsUpdate -Force
+
+# Import the Windows Update module
+Import-Module PSWindowsUpdate
+
+# Check for updates
+Get-WindowsUpdate -AcceptAll -Install -AutoReboot
+
+# Restart the system if updates require a reboot
+Restart-Computer -Force
